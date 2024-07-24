52f211917234fadab54e136dd0fad103799ff6de
Leslie Ajayi <97734325+Leslie-23@users.noreply.github.com>
2024-07-24 04:42:55 +0000

    Update script.js

--- Pomodoro Timer/script.js
+++ Pomodoro Timer/script.js
@@ -1,5 +1,83 @@
+// let timer;
+// let isWorkTime = true;
+
+// function startTimer() {
+//   timer = setInterval(decrementTimer, 1000);
+// }
+
+// function decrementTimer() {
+//   const timeLeftElement = document.getElementById("time-left");
+//   const timeLeft = timeLeftElement.innerHTML.split(":");
+//   let minutes = parseInt(timeLeft[0], 10);
+//   let seconds = parseInt(timeLeft[1], 10);
+
+//   if (minutes === 0 && seconds === 0) {
+//     clearInterval(timer);
+//     playAlarm();
+//     isWorkTime = !isWorkTime;
+//     updateTimerLabel();
+//     resetTimer();
+//     return;
+//   }
+
+//   if (seconds === 0) {
+//     minutes--;
+//     seconds = 59;
+//   } else {
+//     seconds--;
+//   }
+
+//   timeLeftElement.innerHTML = `${minutes < 10 ? "0" : ""}${minutes}:${
+//     seconds < 10 ? "0" : ""
+//   }${seconds}`;
+// }
+
+// function toggleTimer() {
+//   const startStopButton = document.getElementById("start-stop");
+//   if (startStopButton.innerHTML === "Start") {
+//     startStopButton.innerHTML = "Pause";
+//     startTimer();
+//   } else {
+//     startStopButton.innerHTML = "Start";
+//     clearInterval(timer);
+//   }
+// }
+
+// function resetTimer() {
+//   clearInterval(timer);
+//   const startStopButton = document.getElementById("start-stop");
+//   startStopButton.innerHTML = "Start";
+
+//   const timerLabel = document.getElementById("timer-label");
+//   timerLabel.innerHTML = isWorkTime ? "Deep Work" : "Rest";
+
+//   const timeLeftElement = document.getElementById("time-left");
+//   timeLeftElement.innerHTML = isWorkTime ? "25:00" : "05:00";
+// }
+
+// function updateTimerLabel() {
+//   const timerLabel = document.getElementById("timer-label");
+//   timerLabel.innerHTML = isWorkTime ? "Deep Work" : "Rest";
+// }
+
+// function playAlarm() {
+//   const alarm = document.getElementById("alarm");
+//   alarm.play();
+// }
+// function playAlarmReset() {
+//   const alarm = document.getElementById("alarm2");
+//   alarm.play();
+// }
+
 let timer;
 let isWorkTime = true;
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("start-stop").addEventListener("click", toggleTimer);
+  document.getElementById("reset").addEventListener("click", resetTimer);
+  updateTimerLabel();
+  resetTimer();
+});
 
 function startTimer() {
   timer = setInterval(decrementTimer, 1000);
@@ -27,9 +105,7 @@ function decrementTimer() {
     seconds--;
   }
 
-  timeLeftElement.innerHTML = `${minutes < 10 ? "0" : ""}${minutes}:${
-    seconds < 10 ? "0" : ""
-  }${seconds}`;
+  timeLeftElement.innerHTML = `${minutes < 10 ? "0" : ""}${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
 }
 
 function toggleTimer() {
@@ -48,9 +124,6 @@ function resetTimer() {
   const startStopButton = document.getElementById("start-stop");
   startStopButton.innerHTML = "Start";
 
-  const timerLabel = document.getElementById("timer-label");
-  timerLabel.innerHTML = isWorkTime ? "Deep Work" : "Rest";
-
   const timeLeftElement = document.getElementById("time-left");
   timeLeftElement.innerHTML = isWorkTime ? "25:00" : "05:00";
 }
@@ -64,7 +137,9 @@ function playAlarm() {
   const alarm = document.getElementById("alarm");
   alarm.play();
 }
+
 function playAlarmReset() {
   const alarm = document.getElementById("alarm2");
   alarm.play();
 }
+
